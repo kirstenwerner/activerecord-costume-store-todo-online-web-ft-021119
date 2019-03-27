3b43863d10f34425e759a98100e589fc6aaa042f
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-27 00:16:34 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price 
       t.integer :size 
       t.string :image_url
-      t.timestamp
+      t.timestamps
     end
   end 
 end 
