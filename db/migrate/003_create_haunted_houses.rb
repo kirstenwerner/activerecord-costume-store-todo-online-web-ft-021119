1c040dfead290b318a327dd792a4e016835f7a83
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-27 00:14:08 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,12 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.float :location 
+      t.integer :size 
+      t.string :image_url
+      t.timestamp
+    end
+  end 
 end
