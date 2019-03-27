c207c1fe7f3ae4da11bb29ffcd185e11168d88b8
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-27 00:20:23 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,19 +16,17 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.string "location"
     t.integer "costume_inventory"
-    t.integer "num_of_employees"
+    t.integer "number_of_employees"
     t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
-    t.string "name"
+    t.string "names"
     t.float "price"
     t.integer "size"
     t.string "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
@@ -37,7 +35,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "theme"
     t.float "price"
     t.boolean "family_friendly"
-    t.datetime "opening_date"
+    t.datetime "openeing_date"
     t.text "long_description"
   end
 
