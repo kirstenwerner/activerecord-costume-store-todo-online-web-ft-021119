daafa3facaff8513a477d78f1297de16cd31f168
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-27 00:17:47 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -23,12 +23,10 @@ ActiveRecord::Schema.define(version: 3) do
   end
 
   create_table "costumes", force: :cascade do |t|
-    t.string "name"
+    t.string "names"
     t.float "price"
     t.integer "size"
     t.string "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
